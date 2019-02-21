f1a965f03ef055c24d1a3ec443dd185602cd5b1a
fehersanyi-bitrise <sandor.feher@bitrise.io>
2019-02-21 12:17:25 +0100

    removed unnecessary cobra code

--- cmd/root.go
+++ cmd/root.go
@@ -22,9 +22,7 @@ import (
 	"strings"
 
 	"github.com/bitrise-io/go-utils/log"
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -92,22 +90,18 @@ func getIpa(file, path string) (map[string]string, error) {
 	//make a temporal copy
 	_, err := exec.Command("cp", path+"/"+file, path+"/"+newFile).Output()
 	if err != nil {
-		log.Warnf("zipp")
 		return appInfo, err
 	}
 	//create payload
 	_, err = exec.Command("unzip", "-oa", path+"/"+newFile, "-d", path).Output()
 	if err != nil {
-		log.Printf(path + "/" + newFile)
-		log.Warnf("payload")
+
 		return appInfo, err
 	}
-	//cat
+	//read
 	infoPlist := path + "/Payload/" + zip[0] + ".app/Info.plist"
 	infoXML, err := ioutil.ReadFile(infoPlist)
 	if err != nil {
-		log.Printf(infoPlist)
-		log.Warnf("cat")
 		return appInfo, err
 	}
 	infoArray := strings.Split(string(infoXML), "\n")
@@ -185,44 +179,5 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
-	}
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.app-info.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".app-info" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".app-info")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
 	}
 }
