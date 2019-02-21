ffa52c2f668e2984d43ab920e810d36a2dedd939
fehersanyi-bitrise <sandor.feher@bitrise.io>
2019-02-21 09:58:50 +0100

    ipa info implemented

--- cmd/root.go
+++ cmd/root.go
@@ -16,6 +16,7 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"strings"
@@ -36,30 +37,125 @@ var rootCmd = &cobra.Command{
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
-		file, path, err := processPath(args)
-		if err != nil {
-			fmt.Println(err)
+		if len(args) > 0 {
+			file, path := logInfo(args)
+			if strings.Contains(args[0], ".apk") {
+				log.Infof("Retrieving APK Info:")
+				fmt.Println()
+				fmt.Print("APP: ")
+				log.Infof("%s", file)
+				fmt.Print("PATH: ")
+				log.Infof("%s", path)
+				fmt.Println()
+				appInfo, err := getAPK(args[0])
+				if err != nil {
+					log.Errorf("Failed to retrieve APK info: %s", err)
+				}
+				log.Printf("Package name: %s", appInfo["packageName"])
+				log.Printf("Version Code: %s", appInfo["cersionCode"])
+				log.Printf("Version Name: %s", appInfo["versionName"])
+				log.Printf("Path to icon: %s", appInfo["icon"])
+			} else if strings.Contains(args[0], ".ipa") {
+				log.Infof("Retrieving IPA Info:")
+				fmt.Println()
+				fmt.Print("APP: ")
+				log.Infof("%s", file)
+				fmt.Print("PATH: ")
+				log.Infof("%s", path)
+				fmt.Println()
+				logInfo(args)
+				//TODO
+				appInfo, err := getIpa(file, path)
+				if err != nil {
+					log.Errorf("Error retrieving IPA info %s", err)
+					return
+				}
+				fmt.Println()
+				log.Printf("Bundle ID: %s", appInfo["Bundle ID"])
+				log.Printf("Version Number %s: ", appInfo["Version Number"])
+				log.Printf("Build Number: %s", appInfo["Build Number"])
+				log.Printf("Path to icon: %s", appInfo["App Icon"])
+			} else {
+				log.Errorf("Invalid argument: %s", args[0])
+			}
+		} else {
+			log.Errorf("No app provided")
 		}
-		fmt.Print("APP: ")
-		log.Infof("%s", file)
-		fmt.Print("PATH: ")
-		log.Infof("%s", path)
-		fmt.Println()
-		getAPK(args[0])
 	},
 }
 
-func getAPK(path string) {
-	runCmd := []string{"aapt", "dump", "badging"}
-	log.Printf("Running command: ")
-	log.Donef("%s %s", strings.Join(runCmd, " "), path)
+func getIpa(file, path string) (map[string]string, error) {
+	appInfo := make(map[string]string)
+	zip := strings.Split(file, ".")
+	zip[1] = "zip"
+	newFile := strings.Join(zip, ".")
+	//make a temporal copy
+	_, err := exec.Command("cp", path+"/"+file, path+"/"+newFile).Output()
+	if err != nil {
+		log.Warnf("zipp")
+		return appInfo, err
+	}
+	//create payload
+	_, err = exec.Command("unzip", "-oa", path+"/"+newFile, "-d", path).Output()
+	if err != nil {
+		log.Printf(path + "/" + newFile)
+		log.Warnf("payload")
+		return appInfo, err
+	}
+	//cat
+	infoPlist := path + "/Payload/" + zip[0] + ".app/Info.plist"
+	infoXML, err := ioutil.ReadFile(infoPlist)
+	if err != nil {
+		log.Printf(infoPlist)
+		log.Warnf("cat")
+		return appInfo, err
+	}
+	infoArray := strings.Split(string(infoXML), "\n")
+	for i := 0; i < len(infoArray); i++ {
+		if strings.Contains(infoArray[i], "BundleIdentifier") {
+			first := strings.Replace(infoArray[i+1], "<string>", "", -1)
+			appInfo["Bundle ID"] = strings.TrimSpace(strings.Replace(first, "</string>", "", -1))
+		}
+		if strings.Contains(infoArray[i], "BundleShortVersionString") {
+			first := strings.Replace(infoArray[i+1], "<string>", "", -1)
+			appInfo["Version Number"] = strings.TrimSpace(strings.Replace(strings.Replace(first, "</string>", "", -1), ":", "", -1))
+		}
+		if strings.Contains(infoArray[i], "CFBundleVersion") {
+			first := strings.Replace(infoArray[i+1], "<string>", "", -1)
+			appInfo["Build Number"] = strings.TrimSpace(strings.Replace(first, "</string>", "", -1))
+		}
+		if strings.Contains(infoArray[i], "AppIcon-260") {
+			first := strings.Replace(infoArray[i], "<string>", "", -1)
+			appInfo["App Icon"] = strings.TrimSpace(strings.Replace(first, "</string>", "", -1))
+		}
+	}
+	//remove zip
+	_, err = exec.Command("rm", "-f", path+"/"+newFile).Output()
+	if err != nil {
+		return appInfo, err
+	}
+	//remove payload
+	_, err = exec.Command("rm", "-rf", path+"/"+"Payload/").Output()
+	if err != nil {
+		return appInfo, err
+	}
+	return appInfo, nil
+}
+
+func logInfo(args []string) (file, path string) {
+	file, path, err := processPath(args)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return file, path
+}
+
+func getAPK(path string) (map[string]string, error) {
 	command, err := exec.Command("aapt", "dump", "badging", path).Output()
 	if err != nil {
 		fmt.Printf("failed to run command %s", err)
 	}
-
 	info := strings.Split(string(command), "\n")
-
 	appInfo := make(map[string]string)
 	for i := 0; i < len(info); i++ {
 		if strings.Contains(info[i], "package") {
@@ -70,11 +166,7 @@ func getAPK(path string) {
 			appInfo["icon"] = strings.Split(info[i], "'")[1]
 		}
 	}
-
-	log.Printf("Package name: %s", appInfo["packageName"])
-	log.Printf("Version Code: %s", appInfo["cersionCode"])
-	log.Printf("Version Name: %s", appInfo["versionName"])
-	log.Printf("Path to icon: %s", appInfo["icon"])
+	return appInfo, nil
 }
 
 func processPath(args []string) (string, string, error) {
